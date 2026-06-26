4def564d8ee512dc98fec662de93cbed1c3e09b4
shimomo <yuichi@shimomo.net>
2026-06-27 01:23:14 +0900

    chore: update monorepo-builder.php

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
@@ -25,6 +24,5 @@ return static function (MBConfig $mbConfig): void {
         TagVersionReleaseWorker::class,
         PushTagReleaseWorker::class,
         SetNextMutualDependenciesReleaseWorker::class,
-        PushNextDevReleaseWorker::class,
     ]);
 };
