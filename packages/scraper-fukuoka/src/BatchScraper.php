030935cd7396c8d22bc6826d33d6a74a72bbb5e7
shimomo <yuichi@shimomo.net>
2026-08-02 05:33:58 +0900

    refactor!: move bulk methods to BatchScraper

---
+++ packages/scraper-fukuoka/src/BatchScraper.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Turnmark\Scraper\Fukuoka;
+
+use DateTimeInterface;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\NullOutput;
+use Turnmark\Scraper\Scraper as BoatraceScraper;
+
+/**
+ * @author shimomo
+ */
+final class BatchScraper
+{
+    /**
+     * @param \DateTimeInterface|non-empty-string $date
+     * @param list<int<1, 12>> $raceNumbers
+     * @param ?\Symfony\Component\BrowserKit\HttpBrowser $httpBrowser
+     * @return array<int<1, 12>, array<non-empty-string, mixed>>
+     */
+    public static function scrapeTime(
+        DateTimeInterface|string $date,
+        array $raceNumbers = [],
+        ?HttpBrowser $httpBrowser = null,
+    ): array {
+        $response = [];
+
+        $uniqueRaceNumbers = array_unique($raceNumbers ?: BoatraceScraper::getRaceNumbers());
+        $totalSteps = count($uniqueRaceNumbers);
+
+        $output = BoatraceScraper::getShowProgress() ? new ConsoleOutput() : new NullOutput();
+        $output->writeln('<info>📊 オリジナル展示タイムのスクレイピングを開始します</info>');
+        $progressBar = new ProgressBar($output, $totalSteps);
+        $progressBar->setFormat(
+            ' %current%/%max% [%bar%] %percent:3s%% ⏱️ %elapsed:6s% / %estimated:-6s%'
+        );
+        $progressBar->start();
+
+        foreach ($uniqueRaceNumbers as $raceNumber) {
+            $response[$raceNumber] =
+                Scraper::scrapeTime($date, $raceNumber, $httpBrowser);
+
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+        $output->writeln('');
+        $output->writeln("<info>✅ オリジナル展示タイムのスクレイピングが完了しました（{$totalSteps}件）</info>");
+        $output->writeln('');
+
+        return $response;
+    }
+}
---
+++ packages/scraper-fukuoka/tests/BatchScraperTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Turnmark\Scraper\Fukuoka\Tests;
+
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Turnmark\Scraper\Fukuoka\BatchScraper;
+
+/**
+ * @psalm-import-type BatchArguments from \Turnmark\Scraper\Fukuoka\Tests\ScraperPsalmType
+ * @psalm-import-type ExpectedByRaceNumber from \Turnmark\Scraper\Fukuoka\Tests\ScraperPsalmType
+ *
+ * @author shimomo
+ */
+final class BatchScraperTest extends TestCase
+{
+    /**
+     * @param BatchArguments $arguments
+     * @param ExpectedByRaceNumber $expected
+     * @return void
+     */
+    #[Test]
+    #[DataProviderExternal(BatchScraperDataProvider::class, 'scrapeTimeProvider')]
+    public function scrapeTime(array $arguments, array $expected): void
+    {
+        $this->assertSame($expected, BatchScraper::scrapeTime(...$arguments));
+    }
+}
